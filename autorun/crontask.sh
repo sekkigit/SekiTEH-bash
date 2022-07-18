5cccc0f7131df8acc6facbb2a2705ac34188d21b
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-07-18 21:46:21 +0200

    1

---
+++ autorun/crontask.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat <<EOF >> /etc/cron.d/crontask
+0 5 * * *  root    apt update && apt upgrade -y
+EOF
+crontab -u "${SUDO_USER:-$USER}" /etc/cron.d/crontask
---
+++ basic_apps/basic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt update
+apt install cron -y
+apt install nano -y
+apt install btop -y
+apt upgrade -y
